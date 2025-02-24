a96e7fdffe04c2195e128f0662227431766dc5cb
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 21:12:31 +0100

    add test_returns_name_of_shop() (tdd:green)

---
+++ src/main/java/dev/alejandro/models/Shop.java
@@ -0,0 +1,27 @@
+package dev.alejandro.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import dev.alejandro.models.Computer;
+
+public class Shop {
+
+    private String shopName;
+    private String shopOwner;
+    private String shopID;
+    private List<Computer> computers;
+    
+    public Shop(String shopName, String shopOwner, String shopID) {
+        this.shopName = shopName;
+        this.shopOwner = shopOwner;
+        this.shopID = shopID;
+        this.computers = new ArrayList<>();
+    }
+
+    public String getShopName(){
+        return this.shopName;
+    }
+
+
+
+}
