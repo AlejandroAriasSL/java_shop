3b6e3008484e54ad987cc528ebe41ad198ee2984
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 20:39:54 +0100

    test suite of ComputerTest refactored with BeforeEach() (tdd:green)

--- src/test/java/dev/alejandro/models/ComputerTest.java
+++ src/test/java/dev/alejandro/models/ComputerTest.java
@@ -1,5 +1,6 @@
 package dev.alejandro.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,12 +8,18 @@ import static org.hamcrest.Matchers.is;
 
 public class ComputerTest {
 
+    private Computer computer;
+
+    @BeforeEach
+    void setUp(){
+        computer = new Computer("Acer", "12GB", "2.4GHz", "Linux", 2.50);
+    }
+
 
     @Test
     @DisplayName("The getBrand method returns the brand of the computer")
     void test_returns_brand_of_computer(){
 
-        Computer computer = new Computer("Acer", "12GB", "2.4GHz", "Linux", 2.50);
 
         assertThat(computer.getBrand(), is("Acer"));
     }
@@ -21,7 +28,6 @@ public class ComputerTest {
     @DisplayName("The getRAM method returns the RAM of the computer")
     void test_returns_ram_of_computer(){
 
-        Computer computer = new Computer("Acer", "12GB", "2.4GHz", "Linux", 2.50);
 
         assertThat(computer.getRAM(), is("12GB"));
         
