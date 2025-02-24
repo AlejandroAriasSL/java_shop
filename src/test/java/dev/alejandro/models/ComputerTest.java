63d0a4f4c76af88621904ce021cb945bb7406303
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 20:28:01 +0100

    add test_returns_brand_of_computer() (tdd:red)

---
+++ src/test/java/dev/alejandro/models/ComputerTest.java
@@ -0,0 +1,20 @@
+package dev.alejandro.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ComputerTest {
+
+
+    @Test
+    @DisplayName("The getBrand method returns the brand of the computer")
+    void test_returns_brand_of_computer(){
+
+        Computer computer = new Computer("Acer", "12GB", "2.4GHz", "Linux", 2.50);
+
+        assertThat(computer.getBrand(), is("Acer"));
+    }
+
+}
