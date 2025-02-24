18aff695b9ace05061f1fbbe7c9238ad93cd6b11
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 23:27:40 +0100

    add test_removes_computers_of_the_shop_by_brand() (tdd:red)

--- src/test/java/dev/alejandro/controllers/ShopControllerTest.java
+++ src/test/java/dev/alejandro/controllers/ShopControllerTest.java
@@ -2,6 +2,9 @@ package dev.alejandro.controllers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +26,33 @@ public class ShopControllerTest {
 
         assertThat(controller.showShopComputers(), hasItem(computer));
     }
+
+    @Test
+    @DisplayName("Test removeComputerByBrand method removes computer from the shop")
+    void test_removes_computers_of_the_shop_by_brand(){
+
+        Shop shop = new Shop("Mercadona", "Juan Roig", "53423423T");
+        ShopController controller = new ShopController(shop);
+
+        Computer computer = new Computer("Acer", "12GB", "2.4GHz", "Linux", 2.50);
+        Computer computer1 = new Computer("ASUS", "12GB", "2.4GHz", "Linux", 2.50);
+
+        controller.addComputerToShop(computer);
+        controller.addComputerToShop(computer1);
+
+        assertThat(controller.showShopComputers(), hasItem(computer));
+
+        controller.removeByBrand("Acer");
+
+        assertThat(controller.showShopComputers(), not(hasItem(hasProperty("brand", is("Acer")))));
+        
+
+       
+
+        
+    }
+
+
+
 
 }
