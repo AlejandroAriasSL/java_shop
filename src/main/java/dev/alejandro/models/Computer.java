90b83080578d72afdaa1f50132be96d3fc09384d
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 20:32:55 +0100

    add test_returns_brand_of_computer() (tdd:green)

---
+++ src/main/java/dev/alejandro/models/Computer.java
@@ -0,0 +1,26 @@
+package dev.alejandro.models;
+
+public class Computer {
+
+    private final String brand;
+    private final String ram;
+    private final String cpu;
+    private final String os;
+    private final double price;
+    
+    public Computer(String brand, String ram, String cpu, String os, double price) {
+        this.brand = brand;
+        this.ram = ram;
+        this.cpu = cpu;
+        this.os = os;
+        this.price = price;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+    
+
+    
+
+}
