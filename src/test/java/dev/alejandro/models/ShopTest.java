ec6cc305cdd9c1a6d9e1d4d7ded132d24a33a54d
AlejandroAriasSL <alejandroasilva02@gmail>
2025-02-24 21:17:27 +0100

    refactor test_returns_name_of_shop_owner() with beforeEach (tdd:green)

--- src/test/java/dev/alejandro/models/ShopTest.java
+++ src/test/java/dev/alejandro/models/ShopTest.java
@@ -3,16 +3,24 @@ package dev.alejandro.models;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ShopTest {
 
+    private Shop shop;
+
+    @BeforeEach
+    void setUp(){
+        shop = new Shop("Mercadona", "Juan Roig", "53423423T");
+    }
+
+
     @Test
     @DisplayName("The getShopName method returns the name of the shop")
     void test_returns_name_of_shop(){
 
-        Shop shop = new Shop("Mercadona", "Juan Roig", "53423423T");
 
         assertThat(shop.getShopName(), is("Mercadona"));
     }
@@ -20,8 +28,6 @@ public class ShopTest {
     @Test
     @DisplayName("The getShopOwner method returns the name of the owner")
     void test_returns_name_of_shop_owner(){
-
-        Shop shop = new Shop("Mercadona", "Juan Roig", "53423423T");
 
         assertThat(shop.getShopOwner(), is("Juan Roig"));
     }
